ff7921c0b785ef1f2802e39a5b77107f6b745f51
Daniel Liu <mr.picklepinosaur@gmail.com>
2022-03-19 22:48:51 -0400

    building dfa

--- dfa.rs
+++ dfa.rs
@@ -17,29 +17,55 @@ static CONVERSION_TABLE: &'static [(&str, &str)] = &[
 #[derive(Debug)]
 pub struct State<'a> {
     pub accepting: Option<&'a str>,
-    pub transitions: HashMap<&'a str, Box<State<'a>>>
+    pub transitions: HashMap<char, Box<State<'a>>>
 }
 
 #[derive(Debug)]
 pub struct Converter<'a> {
-    pub dfa: State<'a>
+    pub dfa: Box<State<'a>>
+}
+
+impl State<'_> {
+
+    pub fn new() -> Self {
+        State{
+            accepting: None, // empty string does not accept
+            transitions: HashMap::new(),
+        }
+    }
 }
 
 impl Converter<'_> {
 
     pub fn new() -> Self {
         let converter = Converter{
-            dfa: State{
-                accepting: None, // empty string does not accept
-                transitions: HashMap::new(),
-            }
+            dfa: Box::new(State::new()),
         };
-        converter.build_dfa();
-        converter
+        converter.build_dfa()
     }
 
-    fn build_dfa(&self) {
+    fn build_dfa(mut self) -> Self {
+        
+        for conv in CONVERSION_TABLE {
+            let mut cur_state: &mut Box<State> = &mut self.dfa;
+            for (i, ch) in conv.0.chars().enumerate() {
 
+                // create state of does not exist
+                if (!cur_state.transitions.contains_key(&ch)) {
+                    let new_state = State::new();
+                    cur_state.transitions.insert(ch, Box::new(new_state));
+                }
+
+                // transition
+                cur_state = cur_state.transitions.get_mut(&ch).unwrap();
+
+                // mark as accepting if last char
+                if (i == conv.0.len()-1) {
+                    cur_state.accepting = Some(conv.1);
+                } 
+            }
+        }
+        self
     }
 
 }
