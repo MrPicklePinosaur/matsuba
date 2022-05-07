e264c24539d2869b7bbf71fe26855f30d33280a6
Daniel Liu <mr.picklepinosaur@gmail.com>
2022-05-07 00:06:13 -0400

    start cnverting cli to make rpc calls

--- src/bin/matsucli/main.rs
+++ src/bin/matsucli/main.rs
@@ -1,24 +1,7 @@
 
-pub mod matsubaproto {
-    tonic::include_proto!("matsubaproto");
-}
-use tonic::Request;
-use matsubaproto::matsuba_client::MatsubaClient;
-use matsubaproto::{ConvertRequest, ConvertResponse};
-
 mod cli;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-
-    let mut client = MatsubaClient::connect("http://[::1]:10000").await?;
-
-    let response = client.convert(Request::new(
-        ConvertRequest {
-            raw: "konnichiha".to_string(),
-        }
-    )).await?;
-    println!("{:?}", response);
+fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     cli::runcli()?;
 
