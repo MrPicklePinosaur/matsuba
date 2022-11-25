8ebe2bf8aa02d108fa3693c05b2078daee1dd18f
Daniel Liu <mr.picklepinosaur@gmail.com>
2022-11-25 15:57:45 -0500

    wgpl setup

---
+++ matsuba_ime/src/main.rs
@@ -0,0 +1,229 @@
+use log::debug;
+use wgpu::include_wgsl;
+use winit::{
+    dpi::PhysicalPosition,
+    event::*,
+    event_loop::{ControlFlow, EventLoop},
+    platform::unix::WindowBuilderExtUnix,
+    window::{Window, WindowBuilder},
+};
+
+struct State {
+    surface: wgpu::Surface,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    config: wgpu::SurfaceConfiguration,
+    size: winit::dpi::PhysicalSize<u32>,
+    staging_belt: wgpu::util::StagingBelt,
+    glyph_brush: wgpu_glyph::GlyphBrush<()>,
+}
+
+impl State {
+    async fn new(window: &Window) -> Self {
+        let size = window.inner_size();
+
+        // The instance is a handle to our GPU
+        // Backends::all => Vulkan + Metal + DX12 + Browser WebGPU
+        let instance = wgpu::Instance::new(wgpu::Backends::all());
+        let surface = unsafe { instance.create_surface(window) };
+
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::default(),
+                compatible_surface: Some(&surface),
+                force_fallback_adapter: false,
+            })
+            .await
+            .unwrap();
+
+        let (device, queue) = adapter
+            .request_device(
+                &wgpu::DeviceDescriptor {
+                    features: wgpu::Features::empty(),
+                    // WebGL doesn't support all of wgpu's features, so if
+                    // we're building for the web we'll have to disable some.
+                    limits: if cfg!(target_arch = "wasm32") {
+                        wgpu::Limits::downlevel_webgl2_defaults()
+                    } else {
+                        wgpu::Limits::default()
+                    },
+                    label: None,
+                },
+                None, // Trace path
+            )
+            .await
+            .unwrap();
+
+        let render_format = wgpu::TextureFormat::Bgra8UnormSrgb;
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            // format: surface.get_supported_formats(&adapter)[0],
+            format: render_format,
+            width: size.width,
+            height: size.height,
+            present_mode: wgpu::PresentMode::Fifo,
+            alpha_mode: wgpu::CompositeAlphaMode::Auto,
+        };
+
+        surface.configure(&device, &config);
+
+        // set up glyph brush
+        let font = wgpu_glyph::ab_glyph::FontArc::try_from_slice(include_bytes!(
+            "fonts/SourceHanSansJP-Normal.otf"
+        ))
+        .expect("could not load font");
+        let glyph_brush =
+            wgpu_glyph::GlyphBrushBuilder::using_font(font).build(&device, render_format);
+
+        Self {
+            surface,
+            device,
+            queue,
+            config,
+            size,
+            staging_belt: wgpu::util::StagingBelt::new(1024),
+            glyph_brush,
+        }
+    }
+
+    fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+        if new_size.width > 0 && new_size.height > 0 {
+            self.size = new_size;
+            self.config.width = new_size.width;
+            self.config.height = new_size.height;
+            self.surface.configure(&self.device, &self.config);
+        }
+    }
+
+    fn input(&mut self, event: &WindowEvent) -> bool {
+        false
+    }
+
+    fn update(&mut self) {}
+
+    fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
+        let output = self.surface.get_current_texture()?;
+        let view = output
+            .texture
+            .create_view(&wgpu::TextureViewDescriptor::default());
+        let mut encoder = self
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                label: Some("Render Encoder"),
+            });
+
+        let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+            label: Some("Render Pass"),
+            color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                view: &view,
+                resolve_target: None,
+                ops: wgpu::Operations {
+                    load: wgpu::LoadOp::Clear(wgpu::Color {
+                        r: 0.0,
+                        g: 0.0,
+                        b: 0.0,
+                        a: 1.0,
+                    }),
+                    store: true,
+                },
+            })],
+            depth_stencil_attachment: None,
+        });
+        drop(render_pass);
+
+        self.glyph_brush.queue(wgpu_glyph::Section {
+            screen_position: (30., 30.),
+            bounds: (self.size.width as f32, self.size.height as f32),
+            text: vec![wgpu_glyph::Text::new("Hello world ひらがな")
+                .with_color([1.0, 1.0, 1.0, 1.0])
+                .with_scale(40.)],
+            ..wgpu_glyph::Section::default()
+        });
+
+        self.glyph_brush
+            .draw_queued(
+                &self.device,
+                &mut self.staging_belt,
+                &mut encoder,
+                &view,
+                self.size.width,
+                self.size.height,
+            )
+            .expect("Could not draw queued");
+
+        self.staging_belt.finish();
+        self.queue.submit(std::iter::once(encoder.finish()));
+        output.present();
+        self.staging_belt.recall();
+
+        Ok(())
+    }
+}
+
+async fn run() {
+    env_logger::init();
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        // .with_position(position)
+        // .with_inner_size(size)
+        .with_title("highgui")
+        .with_decorations(false)
+        .with_always_on_top(true)
+        .with_resizable(false)
+        .with_override_redirect(true)
+        .build(&event_loop)
+        .unwrap();
+
+    let mut state = State::new(&window).await;
+
+    event_loop.run(move |event, _, control_flow| match event {
+        Event::WindowEvent {
+            ref event,
+            window_id,
+        } => {
+            if window_id == window.id() && !state.input(event) {
+                match event {
+                    WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                    WindowEvent::Resized(physical_size) => {
+                        state.resize(*physical_size);
+                    }
+                    WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                        state.resize(**new_inner_size);
+                    }
+                    _ => {}
+                }
+            }
+        }
+        Event::RedrawRequested(window_id) if window_id == window.id() => {
+            state.update();
+            match state.render() {
+                Ok(_) => {}
+                // Reconfigure the surface if lost
+                Err(wgpu::SurfaceError::Lost) => state.resize(state.size),
+                // The system is out of memory, we should probably quit
+                Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
+                // All other errors (Outdated, Timeout) should be resolved by the next frame
+                Err(e) => eprintln!("{:?}", e),
+            }
+        }
+        Event::MainEventsCleared => {
+            // RedrawRequested will only trigger once, unless we manually
+            // request it.
+            window.request_redraw();
+        }
+        Event::DeviceEvent { device_id, event } => {
+            // events that are recieved regardless of focus
+            match event {
+                DeviceEvent::Key(kb_input) => {
+                    println!("DeviceInput Key {:?}", kb_input);
+                }
+                _ => {}
+            }
+        }
+        _ => {}
+    });
+}
+
+fn main() {
+    pollster::block_on(run());
+}
