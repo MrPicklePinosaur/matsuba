16307b1a9fc27a98b88304f06fe29d72c005e334
Daniel Liu <mr.picklepinosaur@gmail.com>
2022-12-22 14:00:45 -0500

    color theme options

--- matsuba_server/src/config.rs
+++ matsuba_server/src/config.rs
@@ -2,7 +2,7 @@
 use config::{Config, ConfigError, File};
 use lazy_static::lazy_static;
 use pino_xmodmap::KeySym;
-use serde::Deserialize;
+use serde::{de::Visitor, Deserialize};
 use winit::event::VirtualKeyCode;
 
 pub static CACHE_DIR: &str = ".cache/matsuba"; // where the database file goes
@@ -18,11 +18,91 @@ pub struct Settings {
     // pub keys: KeyMap
     pub cache_dir: String,
     pub server: Server,
+    pub theme: Theme,
 }
 
 #[derive(Debug, Deserialize)]
 pub struct Server {
     pub listen_address: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Theme {
+    /// Background color of the window
+    pub bg: Color,
+    /// Text color
+    pub fg: Color,
+    /// Background color for the currently selected completion
+    pub selected_bg: Color,
+    /// Text color for the currently selected completion
+    pub selected_fg: Color,
+    /// Background color for the completion
+    pub completion_bg: Color,
+    /// Text color for the completion
+    pub completion_fg: Color,
+}
+
+#[derive(Debug, Default, Deserialize)]
+pub struct Color {
+    pub r: f32,
+    pub g: f32,
+    pub b: f32,
+    pub a: f32,
+}
+
+impl Color {
+    pub fn new(r: f32, g: f32, b: f32, a: f32) -> Self {
+        Color { r, g, b, a }
+    }
+
+    pub fn from_hex(hexstring: &str) -> Option<Self> {
+        // strip leading # if provided
+        let hexstring = hexstring.trim_start_matches("#");
+
+        if hexstring.len() != 6 && hexstring.len() != 8 {
+            return None;
+        }
+
+        let mut res = [0.0, 0.0, 0.0, 1.0];
+        for i in 0..(hexstring.len() / 2) {
+            let color_val = i64::from_str_radix(&hexstring[i..i + 2], 16);
+            if let Ok(color_val) = color_val {
+                res[i] = (color_val as f32 / 255.0).clamp(0.0, 1.0);
+            }
+        }
+
+        Some(Self {
+            r: res[0],
+            g: res[1],
+            b: res[2],
+            a: res[3],
+        })
+    }
+
+    pub fn as_slice_rgba(&self) -> [f32; 4] {
+        [self.r, self.g, self.b, self.a]
+    }
+
+    pub fn as_slice_rgb(&self) -> [f32; 3] {
+        [self.r, self.g, self.b]
+    }
+}
+
+struct ColorVisitor;
+impl<'de> Visitor<'de> for ColorVisitor {
+    type Value = Color;
+
+    fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+        formatter.write_str("a hex color code (etc #FFFFFF or #550055FF)")
+    }
+
+    /*
+    fn visit_str<E>(self, value: &str) -> Result<Self::Value, E>
+    where E: serde::de::Error
+    {
+
+    }
+    */
 }
 
 /*
@@ -64,5 +144,12 @@ mod tests {
         let settings = Settings::load();
         println!("{:?}", settings);
         assert!(settings.is_ok());
+    }
+
+    #[test]
+    fn color_test() {
+        use super::Color;
+
+        println!("{:?}", Color::from_hex("#ABCDEF"));
     }
 }
